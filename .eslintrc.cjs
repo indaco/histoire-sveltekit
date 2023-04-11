ef7a2f01404c64961eadcf96d46d1810f12525f7
Mirco Veltri <github@mircoveltri.me>
2023-04-11 19:42:30 +0200

    chore: replace eslint-plugin-svelte3 with eslint-plugin-svelte

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,16 +1,31 @@
 module.exports = {
 	root: true,
+	extends: [
+		'eslint:recommended',
+		'plugin:@typescript-eslint/recommended',
+		'plugin:svelte/recommended',
+		'prettier'
+	],
 	parser: '@typescript-eslint/parser',
-	extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'prettier'],
-	plugins: ['svelte3', '@typescript-eslint'],
-	ignorePatterns: ['*.cjs'],
-	overrides: [{ files: ['*.svelte'], processor: 'svelte3/svelte3' }],
-	settings: {
-		'svelte3/typescript': () => require('typescript')
-	},
 	parserOptions: {
 		sourceType: 'module',
-		ecmaVersion: 2020
+		ecmaVersion: 2020,
+		project: ['./tsconfig.json'],
+		extraFileExtensions: ['.svelte']
+	},
+	plugins: ['@typescript-eslint'],
+	ignorePatterns: ['*.js', '*.cjs', '*.spec.ts'],
+	overrides: [
+		{
+			files: ['*.svelte'],
+			parser: 'svelte-eslint-parser',
+			parserOptions: {
+				parser: '@typescript-eslint/parser'
+			}
+		}
+	],
+	settings: {
+		'svelte3/typescript': () => require('typescript')
 	},
 	env: {
 		browser: true,
